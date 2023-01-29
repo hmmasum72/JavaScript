3f7f4d7208441bc7a08cb1926e952780dc9d9ea8
hmmasum72 <hmmasum72@gmail.com>
2023-01-29 20:38:54 +0000

    first commit

---
+++ 18problems.js
@@ -0,0 +1,35 @@
+// problem 1
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// // banana er index ber kora
+// indexNo = fruits.indexOf('Banana')
+// console.log(indexNo)
+// // banana replace with mango
+// fruits[1] = 'Mango'
+// console.log(fruits)
+// // remove orange
+// fruits.pop()
+// console.log(fruits)
+// // add Watermelon
+// fruits.push('Watermelon')
+// console.log(fruits)
+
+// problem 2
+
+var masum = 85;
+var tom = 66;
+var jane =95;
+var peter = 56;
+var john = 40;
+if(masum > 80){
+    console.log('A grade')
+}
+if(masum > 60){
+    console.log('B grade')
+}
+if(masum > 50){
+    console.log('C grade')
+}
+if(masum > 40){
+    console.log('D grade')
+}
